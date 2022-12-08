31f3543767db208172a5de823b77da30fa349932
barrosfabio <fbarros640@gmail.com>
2022-12-07 21:34:30 -0300

    First commit

---
+++ src/test/java/com/space/swapi/SwapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.space.swapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
